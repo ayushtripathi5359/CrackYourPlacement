e4a01c421ddaa01865f12eab4c5d3583d23376d1
ayushtripathi5359 <103111856+ayushtripathi5359@users.noreply.github.com>
2024-07-24 22:31:44 +0530

    Create Merge Sorted Array.cpp

---
+++ Day 2/Merge Sorted Array.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void merge(vector<int>& num1, int m, vector<int>& num2, int n) {
+        int i = m-1;
+        int j = n-1;
+        int k = n+m-1;
+        while(i>=0 && j>=0){
+            if(num1[i]> num2[j]){
+                num1[k]=num1[i];
+                i--,k--;
+            }
+            else{
+                num1[k]=num2[j];
+                j--,k--;
+            }
+        }
+        while(j>=0){
+            num1[k]=num2[j];
+            j--,k--;
+        }
+    }
+};
