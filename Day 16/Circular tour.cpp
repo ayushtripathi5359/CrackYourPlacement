80df4a86886450bda3c632e8c93c68c2325b5191
Ayush Tripathi <103111856+ayushtripathi5359@users.noreply.github.com>
2024-08-07 22:52:16 +0530

    Create Circular tour.cpp

---
+++ Day 16/Circular tour.cpp
@@ -0,0 +1,25 @@
+class Solution{
+  public:
+  
+    int tour(petrolPump p[],int n)
+    {
+       //Your code here
+       int petrolSum = 0;
+       int totalDistance = 0;
+       for(int i = 0 ; i < n ; i++){
+           petrolSum+=(p[i].petrol);
+           totalDistance+=(p[i].distance);
+       }
+       if(totalDistance > petrolSum) return -1;
+       int total = 0;
+       int result = 0;
+       for(int i = 0 ; i < n ; i++){
+           total+=(p[i].petrol - p[i].distance);
+           if(total<0){
+               total = 0;
+               result = i+1;
+           }
+       }
+       return result;
+    }
+};
