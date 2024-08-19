029cd58343496b61546279ce22253503bac797ff
Ayush Tripathi <103111856+ayushtripathi5359@users.noreply.github.com>
2024-08-19 22:40:45 +0530

    Create Binary Tree To DLL.cpp

---
+++ Day 28/Binary Tree To DLL.cpp
@@ -0,0 +1,28 @@
+class Solution {
+  public:
+    Node* bToDLL(Node* root) {
+        // code here
+        stack<Node*> s;
+        Node* ans = new Node(-1);
+        Node* curr = ans, *prev = NULL, *temp = root;
+        while(!s.empty() || temp){
+            if (temp != NULL) {
+                s.push(temp);
+                temp = temp->left;
+            } else {
+                temp = s.top();
+                s.pop();
+                
+                curr->right = new Node(temp->data);
+                curr = curr->right;
+                curr->left = prev;
+                prev = curr;
+                
+                temp = temp->right;
+            }
+        }
+        curr->right = NULL;
+        
+        return ans->right;
+    }
+};
